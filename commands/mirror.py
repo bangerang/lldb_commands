ed887f2339b7a9f762a1aa3498a1f5b1d33f60da
Johan Thorell <johan.thorell@bontouch.com>
2021-01-18 09:38:59 +0100

    Remove unused options

--- commands/mirror.py
+++ commands/mirror.py
@@ -15,19 +15,3 @@ def handle_command(debugger, command, exe_ctx, result, internal_dict):
     debugger.HandleCommand("e -O -- type(of: " + command + ")")
     debugger.HandleCommand("e let $" + name + " = Mirror(reflecting: " + command + ")")
     debugger.HandleCommand('e for child in $' + name + '.children { print("name: \(child.label!), value: \(child.value), type: \(type(of: child.value))") }')
-
-
-def generate_option_parser():
-    usage = "usage: %prog [options] TODO Description Here :]"
-    parser = optparse.OptionParser(usage=usage, prog="mirror")
-    parser.add_option("-m", "--module",
-                      action="store",
-                      default=None,
-                      dest="module",
-                      help="This is a placeholder option to show you how to use options with strings")
-    parser.add_option("-c", "--check_if_true",
-                      action="store_true",
-                      default=False,
-                      dest="store_true",
-                      help="This is a placeholder option to show you how to use options with bools")
-    return parser
--- commands/set_lang.py
+++ commands/set_lang.py
@@ -9,19 +9,3 @@ def __lldb_init_module(debugger, internal_dict):
 def handle_command(debugger, command, exe_ctx, result, internal_dict):
 
     debugger.HandleCommand('settings set target.language {}'.format(command))
-
-
-def generate_option_parser():
-    usage = "usage: %prog [options] TODO Description Here :]"
-    parser = optparse.OptionParser(usage=usage, prog="set_lang")
-    parser.add_option("-m", "--module",
-                      action="store",
-                      default=None,
-                      dest="module",
-                      help="This is a placeholder option to show you how to use options with strings")
-    parser.add_option("-c", "--check_if_true",
-                      action="store_true",
-                      default=False,
-                      dest="store_true",
-                      help="This is a placeholder option to show you how to use options with bools")
-    return parser
