ac6c34e5dafa4285ed412cf070060cbb488509a6
Johan Thorell <johan.thorell@bontouch.com>
2021-03-07 12:48:29 +0100

    Bug fixes in diff command

--- commands/diff.py
+++ commands/diff.py
@@ -9,15 +9,31 @@ import enum
 @lldb.command("diff")
 def handle_diff_command(debugger, expression, ctx, result, internal_dict):
 
-    commands = expression.split("==")
+    if not expression:
+        result.SetError("Expression is empty")
+        return
+
+    command_args = shlex.split(expression, posix=False)
+    parser = generate_option_parser()
+    try:
+        (options, args) = parser.parse_args(command_args)
+    except:
+        result.SetError(parser.usage)
+        return
+
+    commands = re.sub(r'-[\w\W]', '', expression)
+
+    commands = commands.split("==")
 
     interpreter = debugger.GetCommandInterpreter()
 
+    res = lldb.SBCommandReturnObject()
+
     filepaths = []
+
+    set_lock(options, debugger)
     
     for index, command in enumerate(commands):
-        res = lldb.SBCommandReturnObject()
-
         resultWithAssociatedType = run_command(command, debugger)
 
         if resultWithAssociatedType["state"] == "error":
@@ -38,13 +54,21 @@ def handle_diff_command(debugger, expression, ctx, result, internal_dict):
 
     if len(commands) == 1:
         interpreter.HandleCommand("po $persistedFile", res)
-        if res.HasResult() and res.GetOutput().strip() != "empty":
-            filepaths.append(res.GetOutput())
-            interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"empty"', res)
+        if res.HasResult():
+            filepaths.insert(0, res.GetOutput().strip())
+            if options.lock == True:
+                interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"{}"'.format(filepath), res)
+            else:
+                interpreter.HandleCommand('e -l objc -O -- $locked', res)
+                if res.HasResult() and res.GetOutput().strip() == "NO":
+                    interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"{}"'.format(filepath), res)
+                elif res.GetError():
+                    interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"{}"'.format(filepath), res)
+            
         else:
             # Make sure persistedFile exists
-            interpreter.HandleCommand('e -l objc -- NSString *$persistedFile = (NSString*)@"{}"'.format(filepaths[0]), res)
-            interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"{}"'.format(filepaths[0]), res)
+            interpreter.HandleCommand('e -l objc -- NSString *$persistedFile = (NSString*)@"{}"'.format(filepath), res)
+            interpreter.HandleCommand('e -l objc -- $persistedFile = (NSString*)@"{}"'.format(filepath), res)
             return
 
     diff = "git difftool -y --no-index "
@@ -58,6 +82,16 @@ def handle_diff_command(debugger, expression, ctx, result, internal_dict):
 
     os.system(f"chmod +x " + filepath)
     os.system(f"open {filepath}")
+
+def set_lock(options, debugger):
+
+    res = lldb.SBCommandReturnObject()
+    interpreter = debugger.GetCommandInterpreter()
+
+    if options.lock == True:
+        interpreter.HandleCommand('e -l objc -- BOOL $locked = (BOOL)YES;', res)
+    if options.unlock == True:
+        interpreter.HandleCommand('e -l objc -- $locked = (BOOL)NO;', res)
 
 def run_command(command, debugger):
 
@@ -85,3 +119,17 @@ def run_command(command, debugger):
         }
           
         return resultWithAssociatedType
+
+def generate_option_parser():
+    parser = optparse.OptionParser(prog="diff")
+    parser.add_option("-l", "--lock",
+                      action="store_true",
+                      default=False,
+                      dest="lock",
+                      help="Lock diff to output from expression")
+    parser.add_option("-u", "--unlock",
+                      action="store_true",
+                      default=False,
+                      dest="unlock",
+                      help="Unlock diff to output from expression")
+    return parser
