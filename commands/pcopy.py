d6c7c05bf9f930eb4fc2377457efcaadab880157
Johan Thorell <johan.thorell@protonmail.com>
2022-04-01 10:52:25 +0200

    Update pbcopy command

--- commands/pcopy.py
+++ commands/pcopy.py
@@ -16,5 +16,9 @@ def handle_pcopy_command(debugger, expression, ctx, result, internal_dict):
         result.SetError(res.GetError())
         return
     else:
-        subprocess.run("pbcopy", universal_newlines=True, input=res.GetOutput())
+        write_to_clipboard(res.GetOutput())
 
+def write_to_clipboard(output):
+    process = subprocess.Popen(
+        'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+    process.communicate(output.encode('utf-8'))
